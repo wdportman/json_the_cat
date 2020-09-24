0e2ce49794e62efb0581e42c645993fce62e64fe
Will <wdportman@gmail.com>
2020-09-24 23:15:29 +0000

    got logic working between the two functions

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,19 +1,20 @@
 //Users can provide any breed name, causing our application to fetch the information from the API and print out a short description of that breed.
 
-
 const request = require('request');
 
-let url = 'https://api.thecatapi.com/v1/breeds/search?q=';
-let query = process.argv[2];
+const fetchBreedDescription = (breedName, callback) => {
+  request('https://api.thecatapi.com/v1/breeds/search?q=' + breedName, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+    } else {
+      const data = JSON.parse(body);
+      if (data[0] === undefined) {
+        callback(null, "This isn't a cat, sorry!");
+      } else {
+        callback(null, data[0]["description"]);
+      }
+    }
+  })
+};
 
-request(url+query, (error, response, body) => {
-  if (error) {
-    console.log(JSON.parse(error));
-  }
-  if (body === "[]") {
-    console.log("That's not a cat, sorry!");
-  } else {
-    const data = JSON.parse(body);
-    console.log(data[0]["description"]);
-  }
-});
+module.exports = { fetchBreedDescription }
